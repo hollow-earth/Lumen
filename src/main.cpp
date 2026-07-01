d37d3e98a80166dd1f0ff1c30c1e1a17bee71d32
hollow-earth <vanctardi@protonmail.com>
2026-07-01 00:38:36 -0400

    Basic window spawning

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main (){
+int main(){
+    // If GLFW can't be initiated, error
+    if (!glfwInit()){
+        std::cerr << "Unable to initialize GLFW. Exiting." << std::endl;
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Lumen", NULL, NULL);
+
+    // If window somehow couldn't be created, error
+    if (window == nullptr){
+        std::cerr << "Unable to create GLFW window. Exiting." << std::endl;
+        return -1;
+    }
+    
+    std::cin.get(); // Wait for user input before destroying window
+    glfwDestroyWindow(window);
     return 0;
 }
